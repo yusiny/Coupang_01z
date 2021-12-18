1a7f5ae2f02e24e759f83051f8b126dc78d2c603
yusiny <yusinkim.or@gmail.com>
2021-12-18 23:56:00 +0900

    toolbar 만들기1

--- app/src/main/java/com/coupang_olz/coupang_eats/ui/main/home/HomeFragment.kt
+++ app/src/main/java/com/coupang_olz/coupang_eats/ui/main/home/HomeFragment.kt
@@ -1,11 +1,20 @@
 package com.coupang_olz.coupang_eats.ui.main.home
 
+import android.app.Activity
+import androidx.appcompat.app.AppCompatActivity
 import com.coupang_olz.coupang_eats.databinding.FragmentHomeBinding
 import com.coupang_olz.coupang_eats.ui.BaseFragment
+import com.coupang_olz.coupang_eats.ui.main.MainActivity
 
 class HomeFragment(): BaseFragment<FragmentHomeBinding>(FragmentHomeBinding::inflate) {
 
     override fun initAfterBinding() {
-
+        val activity = getActivity() as MainActivity
+        val toolbar = binding.homeToobarTb
+        activity.setSupportActionBar(toolbar)
+        val ab = activity.supportActionBar!!
+        ab.setDisplayShowTitleEnabled(false)
     }
+
+
 }
