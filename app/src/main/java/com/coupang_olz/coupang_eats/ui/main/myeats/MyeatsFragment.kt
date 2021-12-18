b9b87090c66d440354af206d775f6a3622429034
yusiny <yusinkim.or@gmail.com>
2021-12-19 04:25:26 +0900

    8st 모양 완성

--- app/src/main/java/com/coupang_olz/coupang_eats/ui/main/myeats/MyeatsFragment.kt
+++ app/src/main/java/com/coupang_olz/coupang_eats/ui/main/myeats/MyeatsFragment.kt
@@ -3,11 +3,15 @@ package com.coupang_olz.coupang_eats.ui.main.myeats
 import android.content.Intent
 import com.coupang_olz.coupang_eats.databinding.FragmentMyeatsBinding
 import com.coupang_olz.coupang_eats.ui.BaseFragment
+import com.coupang_olz.coupang_eats.ui.login.LoginActivity
 import com.coupang_olz.coupang_eats.ui.siginup.SignUpActivity
 
 class MyeatsFragment(): BaseFragment<FragmentMyeatsBinding>(FragmentMyeatsBinding::inflate) {
 
     override fun initAfterBinding() {
+        binding.myeatsLogin2Tv.setOnClickListener {
+            startActivity(Intent(activity, LoginActivity::class.java))
+        }
         binding.myeatsSignupBtnTv.setOnClickListener{
             startActivity(Intent(activity, SignUpActivity::class.java))
         }
